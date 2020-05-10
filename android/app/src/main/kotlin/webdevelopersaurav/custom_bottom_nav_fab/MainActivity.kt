1d1a5f0306ffbd077ef393c19cf8775780fb3ca7
webDevSaurav <webdevelopersaurav@gmail.com>
2020-05-10 06:09:16 +0530

    first commit

---
+++ android/app/src/main/kotlin/webdevelopersaurav/custom_bottom_nav_fab/MainActivity.kt
@@ -0,0 +1,12 @@
+package webdevelopersaurav.custom_bottom_nav_fab
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
